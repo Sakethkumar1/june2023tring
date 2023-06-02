5ec4b6049f62babed6beade7efa47b90e2323171
Sakethkumar1 <131279456+Sakethkumar1@users.noreply.github.com>
2023-06-02 17:56:35 +0530

    Add files via upload

---
+++ Program206a.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Program206a
+{
+public static void main(String[]args)
+{
+char ch;
+Scanner sc=new scanner(System.in);
+ch=sc.next().charAt(0);
+int ascii_var=(int)ch;
+System.out.println("ASCII of "+ch+"is"+ascii_var);
+//
+}
+}
+
